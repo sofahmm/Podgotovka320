7a82a15006eda2348875f2db9563ce11c615606d
Sofia <ASUS@LAPTOP-DSSEENGE>
2023-10-25 16:12:51 +0300

    Добавьте файлы проекта.

---
+++ Podgotovka320/DbConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podgotovka320
+{
+    public class DbConnection
+    {
+        public static YoxaStudyEntities studyEntities = new YoxaStudyEntities();
+    }
+}
---
+++ Podgotovka320/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Podgotovka320
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<Clothe> products { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new List<Clothe>(DbConnection.studyEntities.Clothe.ToList());
+            this.DataContext = this;
+
+        }
+    }
+}
